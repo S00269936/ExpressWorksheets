e57ad9df495ebdf4c7cba890878bf1726e026d5d
Zac Herrity <S00269936@atu.ie>
2026-09-17 16:26:47 +0100

    finished controller

--- src/controllers/cars.ts
+++ src/controllers/cars.ts
@@ -10,5 +10,19 @@ export class CarController{
             data: `dum dum dummy text, car id is ${_req.params.id}`
         });
     };
-    
+    createCar = async (_req: Request, res: Response): Promise<void> => {
+        res.status(200).json({success:true,
+            data: `dum dum dummy text, car created with name ${_req.body.name}`
+        });
+    };
+    updateCar = async (_req: Request, res: Response): Promise<void> => {
+        res.status(200).json({success:true,
+            data: `dum dum dummy text, car updated with id ${_req.params.id} and name ${_req.body.name}`
+        });
+    };
+    deleteCar = async (_req: Request, res: Response): Promise<void> => {
+        res.status(200).json({success:true,
+            data: `dum dum dummy text, car deleted with id ${_req.params.id}`
+        });
+    };
 }
